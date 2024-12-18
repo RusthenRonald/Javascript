7ac3e94b5184d5c7a7addb92fc935b95e3abd999
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-17 23:45:32 -0300

    testando condições aninhadas

---
+++ ex012.js
@@ -0,0 +1,12 @@
+var idade =13
+if (idade<16){
+    console.log('Não vota')
+}
+else{
+    if (idade>=16 && idade <18){
+        console.log('Voto opicional!')
+    } //se estiver entre 16 e 18 
+    else{
+        console.log('Vota')
+    }
+}
