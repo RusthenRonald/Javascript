3684d8821e53b33cb9b75741e0dba6bb8b55b7e5
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-19 00:02:27 -0300

    refiz todo o programa de bom dia para revisar elif

--- ex013.js
+++ ex013.js
@@ -1,1 +1,14 @@
-
+var hora = new Date().getHours()
+console.log(`Agora são ${hora} horas`)
+if (hora>=0 && hora <5){
+    console.log('Boa Madrugada!')
+}
+else if (hora <12){
+    console.log('Bom Dia!')
+}
+else if (hora >=12 && hora <18){
+    console.log('Boa Tarde!')
+}
+else{
+    console.log('Boa noite!')
+}
