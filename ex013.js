f88a5f2a40f29b6d24aa9d30faac6f7a3b9399a1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-18 23:53:20 -0300

    apaguei para refazer

--- ex013.js
+++ ex013.js
@@ -1,15 +1,1 @@
-var agora = new Date()
-var hora = agora.getHours()
-console.log(`São exatamente ${hora} horas.`)
-if (hora >=0 && hora <6){
-    console.log('Boa madrugada!')
-}
-else if (hora <12){
-    console.log('Bom dia!')
-}
-else if (hora >=12 && hora <18){
-    console.log('Boa tarde!')
-}
-else {
-    console.log('Boa noite!')
-}
+
