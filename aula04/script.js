55013ac6e034473fe62e9e0d4ad36ca0d958ab24
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-19 01:52:00 -0300

    usando condicionais para trocar cor de fundo do site

--- aula04/script.js
+++ aula04/script.js
@@ -1,7 +1,11 @@
 function carregar(){
+    var corpo=window.document.body
     var msg = window.document.getElementById('msg')
     var foto=window.document.getElementById('foto')
     var data= new Date()
     var hora = data.getHours()
     msg.innerHTML=`Agora são ${hora} horas.`
+    if (hora>=0 && hora <5){
+        corpo.style.background = 'darkblue'
+    }
 }
