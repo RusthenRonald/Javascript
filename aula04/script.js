6cbbcd2fc6707f5aab7e2fa279ecc57915e581d6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-19 01:42:56 -0300

    pegando horario atual

--- aula04/script.js
+++ aula04/script.js
@@ -1,4 +1,6 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var foto=window.document.getElementById('foto')
+    var data= new Date()
+    var hora = data.getHours()
 }
