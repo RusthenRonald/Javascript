34eb7237edf8dd878c51f4ebc3aedb4ac1403d45
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-26 17:06:59 -0300

    finalizei

--- testejs.js
+++ testejs.js
@@ -1,15 +1,19 @@
-var hora = 16
+var hora = new Date().getHours()
 if (hora >5 && hora <=11){
     console.log('Bom dia')
+    console.log(`São ${hora} horas`)
 }
 else if (hora >11 && hora<=17){
     console.log('Boa tarde')
+    console.log(`São ${hora} horas`)
 }
 else if (hora >17 && hora <=23){
     console.log('Boa noite')
+    console.log(`São ${hora} horas`)
 }
 else if (hora >0 && hora <=5){
     console.log('Boa madrugada!')
+    console.log(`São ${hora} horas`)
 }
 else{
     console.log('Hora inválida!')
