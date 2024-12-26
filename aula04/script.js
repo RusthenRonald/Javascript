0f6d199e900272eef10c382d5c7b809332ad0b56
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-26 17:48:00 -0300

    adicionei imgs nas condições

--- aula04/script.js
+++ aula04/script.js
@@ -5,11 +5,14 @@ function carregar(){
     msg.innerHTML=`Agora são ${hora} horas!`
     if (hora >=0 && hora <12){
         //bom dia
+        imagem.src='img/manha.png'
     }
     else if (hora>=12 && hora <18){
         //boa tarde
+        imagem.src='img/tarde.png'
     }
     else{
         //boa noite
+        imagem.src='img/noite.png'
     }
 }
