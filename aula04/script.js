735c024136a588716be2caf922c43ef41b66e73e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-26 17:45:25 -0300

    fazendo condições para alterar foto

--- aula04/script.js
+++ aula04/script.js
@@ -3,4 +3,13 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var imagem = window.document.getElementById('imagem')
     msg.innerHTML=`Agora são ${hora} horas!`
+    if (hora >=0 && hora <12){
+        //bom dia
+    }
+    else if (hora>=12 && hora <18){
+        //boa tarde
+    }
+    else{
+        //boa noite
+    }
 }
