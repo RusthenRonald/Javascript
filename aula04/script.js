bd038ba1b070bfac31551b9c3df310283e734305
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-26 17:41:09 -0300

    botei dentro de função

--- aula04/script.js
+++ aula04/script.js
@@ -1,4 +1,6 @@
-var hora = new Date().getHours()
-var msg = window.document.getElementById('msg')
-var imagem = window.document.getElementById('imagem')
-msg.innerHTML=`Agora são ${hora} horas!`
+function carregar(){
+    var hora = new Date().getHours()
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('imagem')
+    msg.innerHTML=`Agora são ${hora} horas!`
+}
