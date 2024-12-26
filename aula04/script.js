0ba0019351e56c939b3a87a9e80e6d5527b22a27
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-26 17:51:04 -0300

    mudei cor dependendo da hora atual

--- aula04/script.js
+++ aula04/script.js
@@ -1,4 +1,5 @@
 function carregar(){
+    var fundo = window.document.body
     var hora = new Date().getHours()
     var msg = window.document.getElementById('msg')
     var imagem = window.document.getElementById('imagem')
@@ -6,13 +7,16 @@ function carregar(){
     if (hora >=0 && hora <12){
         //bom dia
         imagem.src='img/manha.png'
+        fundo.style.background='yellow'
     }
     else if (hora>=12 && hora <18){
         //boa tarde
         imagem.src='img/tarde.png'
+        fundo.style.background='orange'
     }
     else{
         //boa noite
         imagem.src='img/noite.png'
+        fundo.style.background='darkblue'
     }
 }
