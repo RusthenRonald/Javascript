5395ead617b3a26390d8bdde64dc065e62a30320
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-26 23:07:51 -0300

    testando switch

--- testejs.js
+++ testejs.js
@@ -1,20 +1,23 @@
-var hora = new Date().getHours()
-if (hora >5 && hora <=11){
-    console.log('Bom dia')
-    console.log(`São ${hora} horas`)
-}
-else if (hora >11 && hora<=17){
-    console.log('Boa tarde')
-    console.log(`São ${hora} horas`)
-}
-else if (hora >17 && hora <=23){
-    console.log('Boa noite')
-    console.log(`São ${hora} horas`)
-}
-else if (hora >0 && hora <=5){
-    console.log('Boa madrugada!')
-    console.log(`São ${hora} horas`)
-}
-else{
-    console.log('Hora inválida!')
+var dia = 1
+switch(dia){
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log("Terça")
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log("Sexta")
+        break
+    default:
+        console.log("Sabado")
 }
