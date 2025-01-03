16d7b5100a1da3759a8d76be4e0e82c89e39a60d
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-02 23:41:41 -0300

    testando array

--- ex015.js
+++ ex015.js
@@ -1,4 +1,7 @@
 var num = [5,8,2,9,3]
 num[3]=6 //adiciona na posição 3
-num.push(10)//adiciona no final
-console.log(`Nosso vetor é o ${num}`)
+num.push(5)//adiciona no final
+num.sort()//organiza de forma crescente 
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`Nosso primeiro valor é ${num[0]}`)
