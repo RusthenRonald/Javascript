19837d5abf73ecb6756a135faf2fbcdb4e5c538f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-02 23:57:07 -0300

    fiz usando while

--- ex015.js
+++ ex015.js
@@ -1,4 +1,11 @@
 var num = [5,8,2,9,3]
 for(var pos=0;pos<num.length;pos++){
-    console.log()
+    console.log(num[pos])
+}
+// usando while
+var n =[1,2,3,4,5]
+var posi=0
+while (posi<n.length){
+    console.log(n[posi])
+    posi++
 }
