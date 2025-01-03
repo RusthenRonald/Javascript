c657429c1fc951b6f6b2b99e0818b03ec18ac370
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-03 00:18:24 -0300

    testando for in

--- ex015.js
+++ ex015.js
@@ -1,11 +1,4 @@
 var num = [5,8,2,9,3]
-for(var pos=0;pos<num.length;pos++){
+for (var pos in num){
     console.log(num[pos])
-}
-// usando while
-var n =[1,2,3,4,5]
-var posi=0
-while (posi<n.length){
-    console.log(n[posi])
-    posi++
 }
