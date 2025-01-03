b6cc6cfa3767118b497aab4357683a5874bf870a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-02 23:49:02 -0300

    fazendo laço for

--- ex015.js
+++ ex015.js
@@ -1,7 +1,4 @@
 var num = [5,8,2,9,3]
-num[3]=6 //adiciona na posição 3
-num.push(5)//adiciona no final
-num.sort()//organiza de forma crescente 
-console.log(num)
-console.log(`Nosso vetor tem ${num.length} posições`)
-console.log(`Nosso primeiro valor é ${num[0]}`)
+for(var pos=0;pos<num.length;pos++){
+    console.log()
+}
