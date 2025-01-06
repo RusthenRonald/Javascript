cbbb631db39dfedb023141ba99fb74fdd78b5bd6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-05 22:49:08 -0300

    função soma

--- aula04/testejs.js
+++ aula04/testejs.js
@@ -1,9 +1,4 @@
-function parimp(n){
-    if(n%2==0){
-        return 'par'
-    }
-    else{
-        return 'impar'
-    }
+function soma(n1,n2){
+    return n1+n2
 }
-console.log(parimp(9))
+console.log(soma(2,2))
